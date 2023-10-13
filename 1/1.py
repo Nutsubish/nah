57f84c375c27a6dd4ed1a493839df931dfd07210
Nutsubish <147431395+Nutsubish@users.noreply.github.com>
2023-10-13 22:38:36 +0400

    nah j
    just a code f stawt scween nah am stawte

---
+++ 1/1.py
@@ -0,0 +1,25 @@
+#step one fy my game is to make button sta
+import time
+
+property is open 
+
+stawt=open
+
+import token
+token=100
+
+
+stawt=object
+
+open is stawt 
+print(token)
+
+time.sleep(3)
+
+
+
+if stawt is not open: 
+ print("Loading...")
+
+
+ 
